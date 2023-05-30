f4ca4acb8168222ba35b43fb7c6bbbc384ca7161
Garima9654 <76567103+Garima9654@users.noreply.github.com>
2023-05-30 23:59:57 +0530

    Add files via upload

---
+++ week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using std::vector;
+using std::queue;
+
+int bipartite(vector<vector<int> > &adj) {
+  vector<int> color(adj.size(), -1); // initialize color to -1 (unassigned)
+  queue<int> q;
+  for (int s = 0; s < adj.size(); s++) {
+    if (color[s] == -1) { // not yet colored
+      q.push(s);
+      color[s] = 0; // assign color 0 to the starting vertex
+      while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int v : adj[u]) {
+          if (color[v] == -1) { // not yet colored
+            q.push(v);
+            color[v] = 1 - color[u]; // assign opposite color to v
+          } else if (color[v] == color[u]) { // same color as u
+            return 0; // not bipartite
+          }
+        }
+      }
+    }
+  }
+  return 1; // bipartite
+}
+
+int main() {
+  int n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  for (int i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].push_back(y - 1);
+    adj[y - 1].push_back(x - 1);
+  }
+  std::cout << bipartite(adj);
+}
