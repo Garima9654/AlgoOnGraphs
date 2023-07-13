5ac2f43bd83a43d7e7d96afd36fa46404d078f03
Garima9654 <76567103+Garima9654@users.noreply.github.com>
2023-07-13 21:20:36 +0530

    Update bipartite.cpp

--- week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -29,15 +29,4 @@ int bipartite(vector<vector<int> > &adj) {
   return 1; // bipartite
 }
 
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  std::cout << bipartite(adj);
-}
+
