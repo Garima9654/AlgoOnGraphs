ad992cc1b0ae889a44aeba07fe85db07a53c93bb
Garima9654 <76567103+Garima9654@users.noreply.github.com>
2023-10-18 23:45:26 +0530

    Update bipartite.cpp

--- week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -6,7 +6,7 @@ using std::vector;
 using std::queue;
 
 int bipartite(vector<vector<int> > &adj) {
-  vector<int> color(adj.size(), -1); // initialize color to -1 (unassigned)
+  vector<int>color(adj.size(), -1); // initialize color to -1 (unassigned)
   queue<int> q;
   for (int s = 0; s < adj.size(); s++) {
     if (color[s] == -1) { // not yet colored
